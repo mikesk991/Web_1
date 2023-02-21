61d6a7bc0c115ac336f8e8e62e3b6c841c81c1d7
mikesk991 <mikesk991@gmail.com>
2023-02-21 23:02:34 +0700

    5.5.1 Основы каскадных таблицей стилей

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,14 +1,9 @@
-from django.template.response import TemplateResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    header = "Персональные данные"
-    langs = ["Английский","Немецкий","Испанский"]
-    user = {"name": "Максим", "age": 30}
-    addr = ("Виноградная", 23, 45)
-    data = {"header": header, "langs": langs, "user": user, "address": addr}
-    return TemplateResponse(request, "index.html", data)
+    return render(request, "firstapp/home.html")
 
 
 
