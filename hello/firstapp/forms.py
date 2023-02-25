8f76b596186fa8e172fbeb31c694e93719efd081
mikesk991 <mikesk991@gmail.com>
2023-02-26 01:29:37 +0700

    6.3.21 Поле IntegerField для ввода целых чисел

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class UserForm(forms.Form):
-  date_time = forms.SplitDateTimeField(label="Введите дату и время")
+  num = forms.IntegerField(label="Введите целое число")
