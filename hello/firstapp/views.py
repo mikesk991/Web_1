6de4e23da2755974bc7ee357bed8e680a37084ba
mikesk991 <mikesk991@gmail.com>
2023-02-25 23:20:02 +0700

    6.2 Использование в формах POST-запросов

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.http import *
 from .forms import UserForm
-
-# Create your views here.
+from django.shortcuts import render
 
 
 def index(request):
-    userform = UserForm()
-    return render(request, "firstapp/index.html", context={"form": userform})
+    if request.method == "POST":
+        name = request.POST.get ("name") # получить значение поля Имя
+        age = request.POST.get("age")  # получить значение поля Возраст
+        output = f"<h2>Пользователь</h2><h3>Имя - {name},Возраст - {age}</hЗ>"
+        return HttpResponse(output)
+    else:
+        userform = UserForm()
+        return render(request, "firstapp/index.html", {"form": userform})
