852ea8eebd6590c73f4adc42c121ce86f8f80933
mikesk991 <mikesk991@gmail.com>
2023-02-26 00:30:35 +0700

    6.3.9 Поле SlugField для ввода текста

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class UserForm(forms.Form):
-   reg_text = forms.RegexField(label="Текст", regex="^[0-9][A-F][0-9]$")
+   slug_text = forms.SlugField(label="Введите текст")
