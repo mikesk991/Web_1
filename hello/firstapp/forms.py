91ca28be849ce0e90b03fd994228b5d6faf385e4
mikesk991 <mikesk991@gmail.com>
2023-02-26 01:32:56 +0700

    6.3.22 Поле DecimalField для ввода десятичных чисел

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 
 class UserForm(forms.Form):
-  num = forms.IntegerField(label="Введите целое число")
+  num = forms.DecimalField(label="Введите десятичное число",
+                           decimal_places=2)
