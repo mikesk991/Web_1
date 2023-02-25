cee43c36ca912b187a2d21e210f07778cee91fd4
mikesk991 <mikesk991@gmail.com>
2023-02-25 23:35:55 +0700

    6.3.1 Настройка среды для изучения полей разных типов

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,14 +1,7 @@
-from django.http import *
 from .forms import UserForm
 from django.shortcuts import render
 
 
 def index(request):
-    if request.method == "POST":
-        name = request.POST.get ("name") # получить значение поля Имя
-        age = request.POST.get("age")  # получить значение поля Возраст
-        output = f"<h2>Пользователь</h2><h3>Имя - {name},Возраст - {age}</hЗ>"
-        return HttpResponse(output)
-    else:
-        userform = UserForm()
-        return render(request, "firstapp/index.html", {"form": userform})
+    userform = UserForm()
+    return render(request, "firstapp/index.html", {"form": userform})
