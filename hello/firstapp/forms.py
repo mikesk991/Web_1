058378fb5db24a5344622dc20a7d9cf8a210f228
mikesk991 <mikesk991@gmail.com>
2023-02-26 01:04:31 +0700

    6.3.14 Поле FileField для выбора файлов

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -2,7 +2,4 @@ from django import forms
 
 
 class UserForm(forms.Form):
-   file_path = forms.FilePathField(label="Выбирите файл",
-                                   path="c:/Windows/",
-                                   allow_files=True,
-                                   allow_folders=True)
+   file = forms.FileField(label="Файл")
