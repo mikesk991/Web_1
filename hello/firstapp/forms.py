2c68ff1af38710da7e4483b23989698809694bd4
mikesk991 <mikesk991@gmail.com>
2023-02-26 01:16:14 +0700

    6.3.19 Поле DurationField для ввода промежутка времени

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class UserForm(forms.Form):
-   date_time = forms.DateTimeField(label="Введите дату и время")
+  time_delta = forms.DurationField(label="Введите промежуток времени")
