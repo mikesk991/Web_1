8a9e292a17b2788d16d388f0eaea75cb7d63fc64
mikesk991 <mikesk991@gmail.com>
2023-02-26 00:02:36 +0700

    6.3.3 Поле BooleanField для выбора решения: да/нет

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -2,6 +2,5 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    name = forms.CharField(label="Имя клиента")
-    age = forms.IntegerField(label="Возраст клиента")
+   basket = forms.BooleanField(label="Положить товар в корзину", required=False)
 
