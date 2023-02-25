ac966ddcf795430cf7d07e99f1dff074cd25e444
mikesk991 <mikesk991@gmail.com>
2023-02-26 00:37:36 +0700

    6.3.10 Поле URLField для ввода универсального указателя ресурса (URL)

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 
 class UserForm(forms.Form):
-   slug_text = forms.SlugField(label="Введите текст")
+   url_text = forms.URLField(label="Введите URL",
+                             help_text="Например http://www.google.com")
