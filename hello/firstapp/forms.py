b7cd453c9fd3107ec4b43d3d00df4277db5484ee
mikesk991 <mikesk991@gmail.com>
2023-02-26 00:07:10 +0700

    6.3.4 Поле NullBooleanField для выбора решенияЖ да/нет

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class UserForm(forms.Form):
-   basket = forms.BooleanField(label="Положить товар в корзину", required=False)
+   vyb = forms.NullBooleanField(label="Вы поедете в Сочи этим летом?")
 
