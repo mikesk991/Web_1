11985f7137fd56420357f44173f5afba27a01404
mikesk991 <mikesk991@gmail.com>
2023-02-26 21:06:20 +0700

    6.3.26 Поле MultipleChoiceField для выбора данных из списка

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    city = forms.TypedChoiceField(choices=((1, "Москва"),
-                                           (2, "Воронеж"),
-                                           (3, "Курск")),
-                                  label="Выбирите город",
-                                  empty_value=None)
+    country = forms.MultipleChoiceField(choices=((1, "Москва"),
+                                                 (2, "Воронеж"),
+                                                 (3, "Курск"),
+                                                 (4, "Россия")),
+                                        label="Выбирите страны")
