017203f158fd31380fc270e8b8d1480f62ebed93
mikesk991 <mikesk991@gmail.com>
2023-02-26 21:24:22 +0700

    6.4.2 Задание начальных значений полей с помощью свойства initial

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    name = forms.CharField(label="Имя")
-    age = forms.IntegerField(label="Возраст")
+    name = forms.CharField(label="Имя", initial="Введите ФИО")
+    age = forms.IntegerField(label="Возраст", initial=18)
     comment = forms.CharField(label="Комментарий",
                               widget=forms.Textarea)
