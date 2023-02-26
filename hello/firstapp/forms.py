fac82c1d7c0d64f8bf3cc4850ad5871e36ac90d7
mikesk991 <mikesk991@gmail.com>
2023-02-26 23:05:22 +0700

    6.4.8 Присвоение стилей полям формы вручную

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    name = forms.CharField(label="Имя клиента")
-    age = forms.IntegerField(label="Возраст клиента")
+    name = forms.CharField(label="Имя клиента", min_length=3)
+    age = forms.IntegerField(label="Возраст клиента", min_value=1, max_value=100)
 
 
