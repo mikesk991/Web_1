31b0a517fdac7684b769791fe25de32d331fcb88
mikesk991 <mikesk991@gmail.com>
2023-03-01 16:49:49 +0700

    7.6.1 Организация связей между таблицами "один-ко-многим"

--- hello/firstapp/models.py
+++ hello/firstapp/models.py
@@ -7,3 +7,12 @@ class Person(models.Model):
     age = models.IntegerField()
     objects = models.Manager()
     DoesNotExist = models.Manager
+
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+
+class Product(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
+
